fed5f75b3c7e6a9a03cb43ecf74210518a32ea5d
Frederik Smolders <Frederik.Smolders@student.uantwerpen.be>
2015-12-02 22:45:35 +0100

    SpaceImpact Software bijna klaar (kan nog niet dood gaan)

---
+++ src/objects/Lives.java
@@ -0,0 +1,41 @@
+package objects;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+import GUI.GUI;
+
+public class Lives {
+    private int amount;
+    private String note;
+    private ImageIcon sprite;
+    
+    public Lives (int amount, String note, ImageIcon sprite) {
+    	this.amount = amount;
+    	this.note = note;
+    	this.sprite = sprite;
+    }
+    
+    public void draw(Graphics g) {
+        for (int i=amount; i>0; i--)
+        {
+            sprite.paintIcon(null, g, 930 - ((i - 1) * 55), 20);
+        }
+    }
+    
+    public void gedaan(Graphics g) {
+        g.setColor(Color.RED);
+        g.setFont(new Font("TimesRoman", Font.BOLD, 100));
+        g.drawString(note, 215, 375);
+    }
+    
+    public void decrease() {
+    	amount--;
+    }
+    
+    public int getAmount() {
+        return amount;
+    }
+}
